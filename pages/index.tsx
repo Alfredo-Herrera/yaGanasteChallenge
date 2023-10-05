8b2c035acaf56524e4f0b1eb82f83b39916b71ae
Alfredo Herrera <alfredoh@avivacredito.com>
2023-10-05 11:26:32 -0600

    add: get info with SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import MainLayout from '@/layouts/MainLayout/MainLayout';
 import Grid from '@mui/material/Grid';
+import { GetServerSideProps } from 'next';
 import { ReactElement } from 'react';
+import { NextPageWithLayout } from './_app';
 
-const Home = () => {
+type BankListProps = {
+    bankList: BankProps[];
+};
+type BankProps = {
+    description: string;
+    bankName: string;
+    age: number;
+    url: string;
+};
+
+const Home: NextPageWithLayout<BankListProps> = ({ bankList }) => {
+    console.log('🚀 ~ file: index.tsx:7 ~ Home ~ bankList:', bankList);
     return (
         <Grid
             container
@@ -35,6 +48,16 @@ const Home = () => {
         </Grid>
     );
 };
+
+export const getServerSideProps = (async (context) => {
+    const res = await fetch(
+        'https://dev.obtenmas.com/catom/api/challenge/banks'
+    );
+    const repo = await res.json();
+    return { props: { bankList: repo } };
+}) satisfies GetServerSideProps<{
+    bankList: BankListProps;
+}>;
 
 Home.getLayout = function getLayout(page: ReactElement) {
     return <MainLayout title="Dashboard">{page}</MainLayout>;
