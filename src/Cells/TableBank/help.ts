3b8fc9b1988aa95c6fa69593300164634e984c35
Alfredo Herrera <alfredoh@avivacredito.com>
2023-10-05 17:10:20 -0600

    add: table bank

---
+++ src/Cells/TableBank/help.ts
@@ -0,0 +1,28 @@
+import { interfaceHeaderDataTable } from '@/atoms/HeadTable/type';
+
+export const titlesHeader: interfaceHeaderDataTable[] = [
+    {
+        id: 'image',
+        label: 'Imagen',
+        disableMobile: false,
+        align: 'left',
+    },
+    {
+        id: 'name',
+        label: 'Nombre',
+        align: 'left',
+        disableMobile: false,
+    },
+    {
+        id: 'description',
+        label: 'Descripcion',
+        align: 'left',
+        disableMobile: true,
+    },
+    {
+        id: 'anio',
+        label: 'Año',
+        align: 'left',
+        disableMobile: false,
+    },
+];
